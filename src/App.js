77149c556a4541185a874d6fa6759de529e4d772
ismartDevender <me19b024@smail.iitm.ac.in>
2022-08-23 19:39:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import './style.scss';
+import './media-query.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Home from './pages/Home';
+import Details from './pages/Details';
+import { Route, Routes } from 'react-router-dom';
+import AddEditBlog from './pages/AddEditBlog';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Header from './components/Header';
+import Auth from './pages/Auth';
 
 function App() {
+  const [active, setActive] = useState('home');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header setActive={setActive} active={active} />
+      <ToastContainer position="top-center" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Details />} />
+        <Route path="/create" element={<AddEditBlog />} />
+        <Route path="/update/:id" element={<AddEditBlog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/auth" element={<Auth setActive={setActive} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
